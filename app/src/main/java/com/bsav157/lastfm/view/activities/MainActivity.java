0991e66b358d6b4cbb1de8d784a76bd3945e4b81
bsav157 <brayanvargas@wposs.com>
2020-07-19 18:35:04 -0500

    Country search
    -Adding XML resource whit an array of countries
    -Adding spinner with countries
    -Developing the frontend of the search activity (Main Activity)
    -Modifying primary and primary dark colors
    -Adding layout-land for activity_man

--- app/src/main/java/com/bsav157/lastfm/view/activities/MainActivity.java
+++ app/src/main/java/com/bsav157/lastfm/view/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.bsav157.lastfm.view.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.bsav157.lastfm.R;
@@ -17,12 +19,20 @@ public class MainActivity extends AppCompatActivity implements IView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.countries_list);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.countries, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
         Presenter presenter = new Presenter(this);
         presenter.makeApiQuery();
     }
 
     @Override
     public void showData(String result) {
-        
+
     }
 }
